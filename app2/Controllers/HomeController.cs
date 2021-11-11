44f3f68e9dda625f54ca1458dc1c2d7c0d1ad2e5
Rafael Gábrio <rafael-sims@live.com>
2021-11-10 22:16:37 -0300

    first commit

---
+++ app2/Controllers/HomeController.cs
@@ -0,0 +1,26 @@
+﻿using app2.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace app2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ViewResult Index()
+        {
+            return View("inicio");
+        }
+
+        public ViewResult Formulario()
+        {
+            return View("Formulario");
+        }
+    }
+}
---
+++ app2/Models/RespostaConvidados.cs
@@ -0,0 +1,10 @@
+﻿namespace app2.Models
+{
+    public class RespostaConvidados
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }   
+        public string Telefone { get; set; }
+        public bool? Presente { get; set; }
+    }
+}
